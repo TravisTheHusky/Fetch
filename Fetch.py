270dd3652c262aedf618ad0ad6a7021984eda407
TravisTheHusky <bluedog@tutanota.de>
2015-01-26 17:44:08 -0500

    Update Fetch.py
    Added the ability to download a pool with multiple pages

--- Fetch.py
+++ Fetch.py
@@ -8,18 +8,23 @@ import re
 import urllib.request
 import os
 import time
-
 harvest_list = []
 
 #Spider fetches all of the links to all of the images in the pool
-def spider(poolid):
-    url = 'https://e621.net/pool/show/' + str(poolid)
-    source = requests.get(url)
-    source_decoded = source.text
-    soup = BeautifulSoup(source_decoded)
-    for link in soup.findAll('a', {'class': 'tooltip-thumb'}):
-        href = link.get('href')
-        harvest_list.append('https://e621.net/' + href)
+def spider(poolid, pages):
+    current_page = 1
+    while True:
+        if int(pages) < current_page:
+            break
+        url = 'https://e621.net/pool/show/' + str(poolid) + '?page=' + str(current_page)
+        print('Scanning page ' + str(current_page))
+        current_page = current_page + 1
+        source = requests.get(url)
+        source_decoded = source.text
+        soup = BeautifulSoup(source_decoded)
+        for link in soup.findAll('a', {'class': 'tooltip-thumb'}):
+            href = link.get('href')
+            harvest_list.append('https://e621.net/' + href)
 
 #fetch_image downloads the full size image from the page spider provides
 def fetch_image(target_array, pid):
@@ -44,8 +49,9 @@ def fetch_image(target_array, pid):
 print('The Pool ID is the last digits at the end of the url')
 print('Example: https://e621.net/post/show/XXXXX - The Xs would be the Pool ID')
 pid = input('Pool ID: ')
+page_count = input('Number of pages: ')
 print('-----------------------')
-spider(pid)
+spider(pid, page_count)
 numberoflinks = len(harvest_list)
 print(str(numberoflinks) + ' images found... Fetching')
 print('-----------------------')
